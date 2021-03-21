b6dd3c2f8443260b8032a690bf495d142c7dcc73
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2021-03-21 19:05:58 +0500

    3/21/2021

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './views/home';
-
+import Home from './views/route';
+import Blogpost from './views/blogpost'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
 
 
 
   return (
-    <>
-      <Home />
-    </>
+
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/blog">
+          <Blogpost />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
